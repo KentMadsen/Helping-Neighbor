a220d86732eee1b858c116f69ca65f9515704427
Kent vejrup Madsen <20760795+KentMadsen@users.noreply.github.com>
2019-09-01 22:19:58 +0200

    a

--- site/javascript/application.js
+++ site/javascript/application.js
@@ -2,6 +2,12 @@
     // Global Variables
 var global_debug = true
 
+    // entities
+function create_Product()
+{
+
+}
+
     // Parsing of content
 function parse_products( response_body )
 {
@@ -21,17 +27,25 @@ function parse_products( response_body )
 
 function parse_product( response_body )
 {
-    console.log( response_body );
+    console.log( response_body.products );
 
-
-    var x = 0;
+    var x = null;
     
     for( x = 0; 
          x < response_body.products.length; 
          x ++ )
     {
+        console.log( response_body.products[x] );
+
+
 
     }
+
+};
+
+function insert_product( name, description, images, preview)
+{
+    application.products.push();
 
 };
 
@@ -66,7 +80,91 @@ var application = new Vue
         data:
         {
             titel:'Standardlager af stål',
-            products:[]
+            products:
+            [
+                {
+                    Preview:"http://localhost/katalog/data/Hulprofiler/info_kvardratisk_profil_large.jpg",
+                    Titel:'hul profil 1',
+                    Description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet bibendum enim, ut sodales nunc. Donec blandit quam nec purus placerat eleifend. Suspendisse ut nulla dui. Integer orci ante, gravida ullamcorper purus id, varius varius libero. Praesent orci lectus, tempus interdum vehicula eu, ullamcorper at magna. Praesent rutrum nulla ut mi hendrerit cursus',
+
+                    Images:
+                    [
+                        {
+                            url:"http://localhost/katalog/data/Hulprofiler/info_kvardratisk_profil_large.jpg",
+                            preview:"",
+                            alt:""
+                        },
+
+                        {
+                            url:"",
+                            preview:"",
+                            alt:""
+                        },
+                        
+                        {
+                            url:"",
+                            preview:"",
+                            alt:""
+                        } 
+                    ]
+                },
+
+                {
+                    Preview:"http://localhost/katalog/data/Hulprofiler/info_kvardratisk_profil_large.jpg",
+                    Titel:'hul profil 2',
+                    Description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet bibendum enim, ut sodales nunc. Donec blandit quam nec purus placerat eleifend. Suspendisse ut nulla dui. Integer orci ante, gravida ullamcorper purus id, varius varius libero. Praesent orci lectus, tempus interdum vehicula eu, ullamcorper at magna. Praesent rutrum nulla ut mi hendrerit cursus',
+
+                    Images:
+                    [
+                        {
+                            url:"http://localhost/katalog/data/Hulprofiler/info_kvardratisk_profil_large.jpg",
+                            preview:"",
+                            alt:""
+                        },
+
+                        {
+                            url:"",
+                            preview:"",
+                            alt:""
+                        },
+                        
+                        {
+                            url:"",
+                            preview:"",
+                            alt:""
+                        } 
+                    ]
+                },
+                
+                {
+                    Preview:"http://localhost/katalog/data/Hulprofiler/info_kvardratisk_profil_large.jpg",
+                    Titel:'hul profil 3',
+                    Description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet bibendum enim, ut sodales nunc. Donec blandit quam nec purus placerat eleifend. Suspendisse ut nulla dui. Integer orci ante, gravida ullamcorper purus id, varius varius libero. Praesent orci lectus, tempus interdum vehicula eu, ullamcorper at magna. Praesent rutrum nulla ut mi hendrerit cursus',
+
+                    Images:
+                    [
+                        {
+                            url:"http://localhost/katalog/data/Hulprofiler/info_kvardratisk_profil_large.jpg",
+                            preview:"",
+                            alt:""
+                        },
+
+                        {
+                            url:"",
+                            preview:"",
+                            alt:""
+                        },
+                        
+                        {
+                            url:"",
+                            preview:"",
+                            alt:""
+                        } 
+                    ]
+                }
+
+
+            ]
 
         },
         
