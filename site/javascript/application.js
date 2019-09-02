f60bc70eb2876219722de696c7f523ded8642b4c
Kent vejrup Madsen <20760795+KentMadsen@users.noreply.github.com>
2019-09-02 10:25:01 +0200

    a

--- site/javascript/application.js
+++ site/javascript/application.js
@@ -7,51 +7,10 @@ function parse_products( response_body )
 {
     var x = 0;
 
-    arr = []
 
-    // Retrieve individual products, in the katalogs
-    for( x = 0; 
-         x < response_body.products_list.length; 
-         x ++ )
-    {    
-        console.log( response_body.products_list[x] );
-
-        var newArr = null;
-
-        axios.get( response_body.products_list[x].url ).then( response => ( newArr = parse_product( response.data ) ) );
-
-        var y = null;
-
-        for( y = 0; 
-             y < newArr.length; 
-             y++)
-        {
-            arr.push[ newArr[y] ];
-        }
-
-    }
-
-    return arr;
 };
 
-function parse_product( response_body )
-{
-    console.log( response_body.products );
 
-    var arr = [];
-
-    var x = null;
-    
-    for( x = 0; 
-         x < response_body.products.length; 
-         x ++ )
-    {
-        var current = response_body.products[x];
-        arr.push( current );
-    }
-
-    return arr;
-};
 
 // Application
 var application = new Vue
@@ -62,6 +21,8 @@ var application = new Vue
         data:
         {
             titel:'Standardlager af stål',
+            breadcrumbs:[],
+            navigation:[]
             
 
         },
@@ -79,8 +40,7 @@ var application = new Vue
         // Executes on startup
         mounted: function()
         {   
-            axios.get('./data/products.json')
-                 .then( response => ( this.products.push( parse_products( response.data ) ) ) )
+            
         }
     }
 );
